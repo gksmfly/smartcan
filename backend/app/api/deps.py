56a2e7484dcf46d3fee398d9f5ff93157be30448
gksmfly <149565853+gksmfly@users.noreply.github.com>
2025-12-17 20:59:42 +0900

    Update deps.py

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,11 +1,8 @@
 # app/api/deps.py
 
 from typing import Generator
-
 from sqlalchemy.orm import Session
-
 from app.db.session import SessionLocal
-
 
 def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
